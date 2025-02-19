2c664a238edef2da077b9a7130c3dfbe1b8d134d
KoalaWill <108674407+KoalaWill@users.noreply.github.com>
2025-02-19 12:20:21 +0800

    fix slicing and camera follow

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform targetTRANS;
+    public Transform topTriggerTRANS;
+    //public Collider2D endTriggerCOLL;
+    public GameObject target;
+    public Camera camera;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        topTriggerTRANS = GameObject.FindGameObjectWithTag("TOP").GetComponent<Transform>();
+        //endTriggerCOLL = GameObject.FindGameObjectWithTag("END").GetComponent<Collider2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        targetTRANS = target.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(targetTRANS.position.x, Mathf.Min(targetTRANS.position.y,(topTriggerTRANS.position.y-camera.orthographicSize)) + yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);    
+        
+    }
+}
+

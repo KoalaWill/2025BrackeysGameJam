da7241e37597d2df518fffd51579cbac24909374
KoalaWill <108674407+KoalaWill@users.noreply.github.com>
2025-02-23 10:37:20 +0800

    start scene construction finished
    start scene construction finished

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -8,7 +8,7 @@ public class FollowPlayer : MonoBehaviour
     public float FollowSpeed = 2f;
     public float yOffset = 1f;
     public Transform targetTRANS;
-    public Transform topTriggerTRANS;
+    //public Transform topTriggerTRANS;
     //public Collider2D endTriggerCOLL;
     public GameObject target;
     public Camera camera;
@@ -19,7 +19,7 @@ public class FollowPlayer : MonoBehaviour
     void Start()
     {
         camera = GetComponent<Camera>();
-        topTriggerTRANS = GameObject.FindGameObjectWithTag("TOP").GetComponent<Transform>();
+        //topTriggerTRANS = GameObject.FindGameObjectWithTag("TOP").GetComponent<Transform>();
         //endTriggerCOLL = GameObject.FindGameObjectWithTag("END").GetComponent<Collider2D>();
         target = GameObject.FindGameObjectWithTag("Player");
         targetTRANS = target.transform;
@@ -28,7 +28,8 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(targetTRANS.position.x, Mathf.Min(targetTRANS.position.y,(topTriggerTRANS.position.y-camera.orthographicSize)) + yOffset, -10f);
+        //Mathf.Min(targetTRANS.position.y, (topTriggerTRANS.position.y - camera.orthographicSize))
+        Vector3 newPos = new Vector3(targetTRANS.position.x, targetTRANS.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);    
         
     }

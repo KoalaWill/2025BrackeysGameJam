fd20452c6c36e38f9b7270bab7e29864bb105f77
KoalaWill <108674407+KoalaWill@users.noreply.github.com>
2025-02-25 21:50:42 +0800

    ...

--- Assets/Scripts/SceneItems/Scene3/GameSuccessBack.cs
+++ Assets/Scripts/SceneItems/Scene3/GameSuccessBack.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameSuccessBack : MonoBehaviour
 {
+
+    private SpriteRenderer spriteRenderer;
+    private Color color;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        color = spriteRenderer.color;
+        // the alpha value 
+        color.a = 0f;
+        spriteRenderer.color = color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.instance.currentState == GameManager.GameState.EndLevel && GameManager.instance.level == 3) gameObject.SetActive(false);
+        if (GameManager.instance.currentState == GameManager.GameState.EndLevel && GameManager.instance.level == 3) {
+            color.a = 0.5f;
+            spriteRenderer.color = color;
+        }
     }
 }
